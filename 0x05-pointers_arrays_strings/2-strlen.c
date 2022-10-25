56e5709415d58ec73f82a67467ae8f4747404e31
samtemmy <iambamideletemidayo@gmail.com>
2022-10-25 05:20:01 -0700

    work edit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,11 +4,11 @@
  * @s: input string
  * *Return: length of a string
  **/
-int _strlen(char*s)
+int _strlen(char *s)
 {
 	int count = 0;
 
 	while (*(s + count) != '\0')
-		count ++;
+		count++;
 	return (count);
 }
