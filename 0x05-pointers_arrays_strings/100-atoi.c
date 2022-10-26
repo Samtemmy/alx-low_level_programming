3c3c1ff1a3d2f1365d0dbbf099d00fb953a4652e
samtemmy <iambamideletemidayo@gmail.com>
2022-10-25 22:36:31 -0700

    work almost done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _atoi - converts a string into an integer.
+ * _atoi - converts a string into an integer
  * @s: input string
  * Return: integer
  **/
@@ -10,21 +10,21 @@ int _atoi(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		if (size > 0 && (*(s + count) < '0' || (s + count < '9')))
-				break;
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
 		if (*(s + count) == '-')
 			pn *= -1;
 		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
-		if (size > 0)
-			m *= 10;
-		size++;
+			if (size > 0)
+				m *= 10;
+			size++;
 		}
 		count++;
 	}
 		for (i = count - size; i < count; i++)
 		{
-			oi = oi + ((*s + i) - 48 * m);
+			oi = oi + ((*(s + i) - 48) * m);
 			m /= 10;
 		}
 		return (oi * pn);
